24313eb71c06ea5a44d3dd3bdd60bdd38d98b444
Oleksii <okurdysh@cern.ch>
2024-02-01 11:19:54 +0100

    longer grid certif

--- rivetSetup.sh
+++ rivetSetup.sh
@@ -6,6 +6,7 @@ source setupRivet
 
 lsetup "panda 1.5.68"
 lsetup "rucio -w"
+voms-proxy-init -voms atlas --valid 48:0 # 48h grid certif and not 12h default
 
 source /exp/atlas/kurdysh/vbs_cross_terms_study/python_packages/setup.sh # this is for sympy installed with pipInstall
 source /exp/atlas/kurdysh/vbs_cross_terms_study/python_package2/setup.sh # for plotting modules of sympy
