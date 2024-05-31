d4b7991318e82c00112d4970ff5b4897e3aa0c0c
Oleksii <okurdysh@cern.ch>
2024-05-31 19:31:05 +0200

    irrelevant commit

--- Wy_lvy_run.sh
+++ Wy_lvy_run.sh
@@ -1,4 +1,4 @@
-run_1_download_0=1
+run_1_download_0=0
 ############## Wy
-python run_chain_all_rivets.py --tGenProd "Wmy" --tGenDec "lvy"  --runQUAD 1  --runLocally 0 --routine "Wy_John" --cut "SR" --runRivet $run_1_download_0
-python run_chain_all_rivets.py --tGenProd "Wpy" --tGenDec "lvy"  --runQUAD 1  --runLocally 0 --routine "Wy_John" --cut "SR" --runRivet $run_1_download_0
+python run_chain_all_rivets.py --tGenProd "Wmy" --tGenDec "lvy"  --runQUAD 1  --runLocally 0 --routine "Wy_John" --cut "SR" --runRivet $run_1_download_0 --doMakeHtml 1
+python run_chain_all_rivets.py --tGenProd "Wpy" --tGenDec "lvy"  --runQUAD 1  --runLocally 0 --routine "Wy_John" --cut "SR" --runRivet $run_1_download_0 --doMakeHtml 1
--- ws_inspect.py
+++ ws_inspect.py
@@ -9,15 +9,15 @@ opts, _ = parser.parse_args()
 bin_pref = {"zvvy": "pred_measurement_", "wz": "pred_measurement_Run2_mTWZvsBDT_"}
 n_bins = {"zvvy": 8, "wz": 20}
 model = {"zvvy": "clip100_pdf", "wz": "EFT_aQGCnew_allsys_Run2_MtWZ_mJJ_M1_expected_quad_statOnly_unscaled_pdf"}
-f = ROOT.TFile(f"../workspaces/{opts.prod_dec}_unclip.root" )
+f = ROOT.TFile(f"../workspaces/{opts.gen_prod_dec}_unclip.root")
 
 # Retrieve workspace from file
 w = f.Get("w")
 
-model = w[model[opts.prod_dec]]
+model = w[model[opts.gen_prod_dec]]
 if opts.print_model=="yes": model.Print("t")
 
 if opts.print_bins=="yes":
-    for binno in range(n_bins[opts.prod_dec]):
-        myvar = w[f"{bin_pref[opts.prod_dec]}{binno}"]
+    for binno in range(n_bins[opts.gen_prod_dec]):
+        myvar = w[f"{bin_pref[opts.gen_prod_dec]}{binno}"]
         myvar.Print("v")
