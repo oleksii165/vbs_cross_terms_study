231e7e4dcb391b8d9bd580c8361ded58644610e6
Oleksii <okurdysh@cern.ch>
2024-06-11 15:55:29 +0200

    test

--- WZ_lllv_run.sh
+++ WZ_lllv_run.sh
@@ -1,4 +1,4 @@
 run_1_download_0=0
 ############## WZ
-python run_chain_all_rivets.py --tGenProd "WmZ" --tGenDec "lllv" --runQUAD 1 --runLocally 0 --routine "WZ_lllv" --cut "SR" --runRivet $run_1_download_0
-python run_chain_all_rivets.py --tGenProd "WpZ" --tGenDec "lllv" --runQUAD 1 --runLocally 0 --routine "WZ_lllv" --cut "SR" --runRivet $run_1_download_0
+python run_chain_all_rivets.py --tGenProd "WmZ" --tGenDec "lllv" --runINT 1 --runCROSS 1 --runLocally 0 --routine "WZ_lllv" --cut "SR" --runRivet $run_1_download_0
+python run_chain_all_rivets.py --tGenProd "WpZ" --tGenDec "lllv" --runINT 1 --runCROSS 1 --runLocally 0 --routine "WZ_lllv" --cut "SR" --runRivet $run_1_download_0
