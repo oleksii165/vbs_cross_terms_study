93183fa624ce1ec42168fdc44f576a1df2711b2d
Oleksii Kurdysh <11kurdysh@gmail.com>
2024-10-12 13:23:55 +0200

    clean up and update to LAPP cluster env setup

--- rivetSetup.sh
+++ rivetSetup.sh
@@ -1,19 +1,15 @@
 # for json in c++
-lsetup git
-cd /exp/atlas/kurdysh/vbs_cross_terms_study/vcpkg/
+cd ../vcpkg
 ./bootstrap-vcpkg.sh
 ./vcpkg install nlohmann-json
-cd /exp/atlas/kurdysh/vbs_cross_terms_study/plotting/
+cd ../vbs_cross_terms_study
 
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh 
+setupATLAS # defined in startup bash script
+lsetup git
 
-asetup 23.6.22,AthGeneration
+asetup 23.6.26,AthGeneration # el9
 source setupRivet
 
 lsetup "panda"
 lsetup "rucio -w"
-voms-proxy-init -voms atlas --valid 48:0 # 48h grid certif and not 12h default
-
-source /exp/atlas/kurdysh/vbs_cross_terms_study/python_packages/setup.sh # this is for sympy installed with pipInstall
-source /exp/atlas/kurdysh/vbs_cross_terms_study/python_package2/setup.sh # for plotting modules of sympy 
+voms-proxy-init -voms atlas --valid 48:0 # 48h grid certif and not 12h defaulti
